b4e566048a1302bb8c023746ebc4ced8833c40df
mustafa137608064 <168101748+mustafa137608064@users.noreply.github.com>
2025-05-03 23:17:19 +0330

    Add files via upload

---
+++ message2add.php
@@ -0,0 +1,29 @@
+<?php
+
+// دریافت اطلاعات از فرم
+$user_name = $_POST['user_name'];
+$message2 = $_POST['message2'];
+
+// اتصال به پایگاه داده
+$db_connection = mysqli_connect('fdb1031.runhosting.com', '4428161_db', 'mustafa1234', '4428161_db');
+
+$check_query = "SELECT * FROM messages WHERE user_name = '$user_name'";
+$result = mysqli_query($db_connection, $check_query);
+
+if (mysqli_num_rows($result) > 0) {
+    $sql_query = "UPDATE messages SET message2 = '$message2' WHERE user_name = '$user_name'";
+} else {
+    $sql_query = "INSERT INTO messages (user_name, message2) VALUES ('$user_name', '$message2')";
+}
+mysqli_query($db_connection, $sql_query);
+
+// بستن اتصال به پایگاه داده
+mysqli_close($db_connection);
+
+
+// پیغام موفقیت آمیز
+  echo "<script>";
+  echo "alert('" . $message2 . " بعنوان پیغام شماره 2 برای " . $user_name . " ثبت شد');";
+  echo "window.location.href = 'user.php?name=" . $user_name . "';";
+  echo "</script>";
+?>
---
+++ message3add.php
@@ -0,0 +1,28 @@
+<?php
+
+// دریافت اطلاعات از فرم
+$user_name = $_POST['user_name'];
+$message3 = $_POST['message3'];
+
+// اتصال به پایگاه داده
+$db_connection = mysqli_connect('fdb1031.runhosting.com', '4428161_db', 'mustafa1234', '4428161_db');
+
+$check_query = "SELECT * FROM messages WHERE user_name = '$user_name'";
+$result = mysqli_query($db_connection, $check_query);
+
+if (mysqli_num_rows($result) > 0) {
+    $sql_query = "UPDATE messages SET message3 = '$message3' WHERE user_name = '$user_name'";
+} else {
+    $sql_query = "INSERT INTO messages (user_name, message3) VALUES ('$user_name', '$message3')";
+}
+mysqli_query($db_connection, $sql_query);
+
+// بستن اتصال به پایگاه داده
+mysqli_close($db_connection);
+
+// پیغام موفقیت آمیز
+  echo "<script>";
+  echo "alert('" . $message3 . " بعنوان پیغام شماره 3 برای " . $user_name . " ثبت شد');";
+  echo "window.location.href = 'user.php?name=" . $user_name . "';";
+  echo "</script>";
+?>
