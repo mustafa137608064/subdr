ec13cd8aa4734c2f3d8a6caa0039477295869c2b
mustafa137608064 <168101748+mustafa137608064@users.noreply.github.com>
2025-05-05 18:12:45 +0330

    Upload 09303382499.php

---
+++ users/09303382499.php
@@ -0,0 +1,229 @@
+<html><body><br><div></div><br></body></html>
+<html><body><br><div></div><br></body></html><html><body><br><div></div><br></body></html>
+<html><body><br><div></div><br></body></html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vless://telegram-id-ArV2ray@speedtest.net:443?encryption=none&security=tls&sni=speedtest.net&alpn=h2%2Chttp%2F1.1&fp=chrome&type=ws&host=dwojwh8.ir&path=%2Ftelegram-ArV2ray%2Ctelegram-ArV2ray%2Ctelegram-ArV2ray%2Ctelegram-ArV2ray%2Ctelegram-ArV2ray%2Ctelegram-ArV2ray%2Ctelegram-ArV2ray%2Ctelegram-ArV2ray%2Ctelegram-ArV2ray%2Ctelegram-ArV2ray%3Fed%3D2048#%F0%9F%97%93%EF%B8%8F%20%DB%8C%DA%A9%D8%B4%D9%86%D8%A8%D9%87%2014%20%D8%A7%D8%B1%D8%AF%DB%8C%D8%A8%D9%87%D8%B4%D8%AA%201404%20%F0%9F%97%93%EF%B8%8F
+<html><body><br><div></div><br></body></html>
+<html><body><br><div></div><br></body></html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vless://21c1c5e6-52fd-4e63-96d9-a7ec002f60aa@192.168.1.1:1111?mode=gun&security=tls&encryption=none&alpn=h2&fp=chrome&type=grpc&serviceName=#📊 زمان اتمام سرویس: 22 روز دیگر 📊
+<html><body><br><div></div><br></body></html>
+<html><body><br><div></div><br></body></html><?php
+if (file_exists('../config.php')) {
+    include '../config.php';
+} else {
+    die('Error: Config file not found.');
+}
+$user_name = basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+if (substr($user_name, -4) === '.php') {
+    $user_name = substr($user_name, 0, -4);
+}
+$connshowmessage = new mysqli($servername, $username, $password, $dbname);
+if ($connshowmessage->connect_error) {
+    die("Connection failed: " . $connshowmessage->connect_error);
+}
+$sqlshowmessage = "SELECT message1, message2, message3 FROM messages WHERE user_name = ?";
+$stmt = $connshowmessage->prepare($sqlshowmessage);
+if ($stmt === false) {
+    die("Prepare failed: " . $connshowmessage->error);
+}
+$stmt->bind_param("s", $user_name);
+$stmt->execute();
+$resultshowmessage = $stmt->get_result();
+
+if ($resultshowmessage->num_rows > 0) {
+    $row = $resultshowmessage->fetch_assoc();
+    if (isset($row['message1'])) {
+        echo "\n";
+        echo htmlspecialchars($row['message1'], ENT_QUOTES, 'UTF-8');
+        echo "\n";
+    } else {
+        echo "\n"; 
+        echo "No message found."; 
+        echo "\n"; 
+    }
+} else {
+    echo "\n"; 
+    echo "No records found.";
+    echo "\n"; 
+}
+$stmt->close();
+$connshowmessage->close();
+?><html><body><br><div></div><br></body></html>
+<html><body><br><div></div><br></body></html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vless://b4bab86a-2418-483e-8e88-57e6f0db333a@1s2.romil-ltd.co.uk:443?security=reality&alpn=http%2F1.1&encryption=none&pbk=W5jAswnd_wfiaDerY2yy3zIyNUbWoks2-tmkAFOE7VA&headerType=None&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=snapp.ir&sid=d77fdb611c4c#%D8%A7%DB%8C%D8%B1%D8%A7%D9%86%D8%B3%D9%84%20%F0%9F%87%AB%F0%9F%87%AE
+vmess://eyJhZGQiOiIxczEucm9taWwtbHRkLmNvLnVrIiwiYWlkIjoiMCIsImFscG4iOiJodHRwLzEuMSIsImZwIjoiY2hyb21lIiwiaG9zdCI6ImZhc3QuY29tIiwiaWQiOiJiNGJhYjg2YS0yNDE4LTQ4M2UtOGU4OC01N2U2ZjBkYjMzM2EiLCJuZXQiOiJ0Y3AiLCJwYXRoIjoiLzdMMUs4WndiMmtnUHB5YjZBS3hJTXgiLCJwb3J0IjoiODAiLCJwcyI6Itin24zYsdin2YbYs9mEIPCfh6vwn4euIiwic2N5IjoiYXV0byIsInNuaSI6ImZhc3QuY29tIiwidGxzIjoiIiwidHlwZSI6Imh0dHAiLCJ2IjoiMiJ9
+
+<html><body><br><div></div><br></body></html>
+<html><body><br><div></div><br></body></html>
